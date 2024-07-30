b5047ef82949458645798022586a2d1ed320463d
Dmitriy Ivanov <103880032+dmlorren@users.noreply.github.com>
2024-07-30 23:24:49 +0300

    Update WelcomerTest.java

--- src/test/java/plaindoll/WelcomerTest.java
+++ src/test/java/plaindoll/WelcomerTest.java
@@ -30,4 +30,9 @@ public class WelcomerTest {
 	public void welcomerSaysSomething(){
 		assertThat(welcomer.saySome(), containsString("something"));
 	}
+	@Test
+	public void welcomersayByeHuner(){
+		assertThat(welcomer.sayByeHunter(), containsString("hunter"));
+	}
+	
 }
